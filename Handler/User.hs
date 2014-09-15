03c3cfb1bc9521aa4051832c923e95017095dc94
kRITZCREEK <hegi-power@web.de>
2014-09-15 14:58:17 +0200

    Try to get MongoDB Connections up and running.

--- Handler/User.hs
+++ Handler/User.hs
@@ -1,7 +1,14 @@
 {-# LANGUAGE TupleSections, OverloadedStrings #-}
 module Handler.User where
 
+{-# LANGUAGE QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell,
+OverloadedStrings, GADTs, FlexibleContexts #-}
+
+
 import Import
+
+import Database.Persist.Types (PersistValue(PersistInt64))
+import Database.Persist.Class (PersistEntity(Key))
 
 getUserR :: String -> Handler Value
 getUserR str = do
@@ -11,4 +18,7 @@ getUserR str = do
 postUserR :: String -> Handler Value
 postUserR str = do
   body <- requireJsonBody
-  return body
+  result <- runDB $ Key $ PersistInt64 (fromIntegral 1)
+  return $ case result of
+    Just val -> val
+    Nothing -> "This was a triumph im making a note here huge success"
